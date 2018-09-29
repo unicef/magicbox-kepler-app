ef4efd9aed4a9404cf424a02e79c434b537ccb26
mikefab <mikefabrikant@gmail.com>
2018-09-28 21:13:18 -0400

    Change config to json so can be easily replaced later

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const config = require('./config-sample.json')
+const config = require('./config')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
