44ac0919d089f8b997765fd5c6df0b2003fb8b98
Thoa T <thoat@users.noreply.github.com>
2018-11-10 22:52:22 -0500

    Set up ESLint for both /client and /server
    Also, by convenience, lint /client/webpack.config.js and  /server/app.js

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ const jsonfile = require('jsonfile');
 
 const blobFetcher = require('./azure/blob-fetcher');
 const config = require('./config');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+const router = express.Router(); // get an instance of the express Router
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -15,9 +17,8 @@ app.use((req, res, next) => {
 });
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.json({ limit: '250mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }))
-var router = express.Router();              // get an instance of the express Router
+app.use(bodyParser.json({ limit: '250mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }));
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', (req, res) => {
@@ -31,39 +32,37 @@ router.get('/default', (req, res) => {
 
 router.route('/save')
   .post((req, res) => {
-    jsonfile.writeFile('config.json', req.body, err => {
-      if (err) {
-        res.send({ message: 'Could not save' });
-      } else {
+    jsonfile.writeFile('config.json', req.body, (err) => {
+      if (err) res.send({ message: 'Could not save' });
+      else {
         res.send({
-          message: 'Saved! You may need to repoen your ' +
-            'browser in incognito mode next time you retrieve.'
+          message: 'Saved! You may need to repoen your '
+            + 'browser in incognito mode next time you retrieve.',
         });
       }
-    })
+    });
   });
 
 router.get('/countries', (req, res) => {
   blobFetcher.listBlobs()
     .then(result => res.send(result));
-})
+});
 
 router.get('/countries/:countryCode/:adminLevel', (req, res) => {
-  let blobName = req.params.countryCode + "_" + req.params.adminLevel + ".json";
-  console.log(blobName);
+  const blobName = `${req.params.countryCode}_${req.params.adminLevel}.json`;
   blobFetcher.saveBlob(blobName)
-    .then(res => JSON.parse(res)) // transform from text string to json object
+    .then(response => JSON.parse(response)) // transform from text string to json object
     .then(result => res.send(result));
-})
+});
 
 router.get('/:user/samples', (req, res) => {
-  let userFolder = req.params.user;
+  const userFolder = req.params.user;
   jsonfile.readFile(`./${userFolder}/samples.json`, (err, obj) => {
-    if (err) console.log(err);
-    res.send(obj);
+    if (err) res.send({ message: 'Could not load sample maps' });
+    else res.send(obj);
   });
-})
+});
 
 app.use('/api', router);
 
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, () => console.log(`Listening on port ${port}...`));
