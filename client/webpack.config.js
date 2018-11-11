45e118c83d7666165cd7a33e8d40767fa0cd5fc2
Thoa T <thoat@users.noreply.github.com>
2018-11-10 22:52:22 -0500

    Set up ESLint for both /client and /server
    Also, by convenience, lint /client/webpack.config.js and  /server/app.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,21 +22,23 @@
 // delete the local development overrides at the bottom of this file
 
 // avoid destructuring for older Node version support
+/* eslint-disable prefer-destructuring */
 const resolve = require('path').resolve;
 const join = require('path').join;
 const webpack = require('webpack');
+/* eslint-enable prefer-destructuring */
 
 const CONFIG = {
   // bundle app.js and everything it imports, recursively.
   entry: {
-    app: resolve('./src/main.js')
+    app: resolve('./src/main.js'),
   },
 
   devtool: 'source-map',
 
   resolve: {
     // Make src files outside of this dir resolve modules in our node_modules folder
-    modules: [resolve(__dirname, '.'), resolve(__dirname, 'node_modules'), 'node_modules']
+    modules: [resolve(__dirname, '.'), resolve(__dirname, 'node_modules'), 'node_modules'],
   },
 
   module: {
@@ -45,28 +47,27 @@ const CONFIG = {
         test: /\.js$/,
         loader: 'babel-loader',
         include: join(__dirname, 'src'),
-        exclude: [/node_modules/]
+        exclude: [/node_modules/],
       },
       {
         // The example has some JSON data
         test: /\.json$/,
         loader: 'json-loader',
-        exclude: [/node_modules/]
-      }
-    ]
+        exclude: [/node_modules/],
+      },
+    ],
   },
 
   node: {
-    fs: 'empty'
+    fs: 'empty',
   },
 
   // Optional: Enables reading mapbox token from environment variable
   plugins: [
-    new webpack.EnvironmentPlugin(['MapboxAccessToken'])
-  ]
+    new webpack.EnvironmentPlugin(['MapboxAccessToken']),
+  ],
 };
 
 // This line enables bundling against src in this repo rather than installed deck.gl module
-module.exports = env => {
-  return env ? require('../webpack.config.local')(CONFIG, __dirname)(env) : CONFIG;
-};
+/* eslint-disable-next-line global-require, import/no-unresolved */
+module.exports = env => (env ? require('../webpack.config.local')(CONFIG, __dirname)(env) : CONFIG);
