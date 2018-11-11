0dedbd143df71dbc81f7de02f6b30fde6001107e
mikefab <mikefabrikant@gmail.com>
2018-11-11 11:38:23 -0500

    camelcase

--- server/azure/config-sample.js
+++ server/azure/config-sample.js
@@ -7,12 +7,12 @@ module.exports = {
   connectionSettings: {
     host: 'ip_address',
     port: 22, // normal is port 22
-    username: [fill yours here],
-    password: [fill yours here]
+    username: 'username',
+    password: 'fill yours here'
   },
   azure: {
-    storage_account: 'storage_account_name',
+    storageAccount: 'storage_account_name',
     key1: 'storage_key',
-    countainer_name: 'gadm2-8'
+    containerName: 'gadm2-8'
   }
 }
