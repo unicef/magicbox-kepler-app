1bb4040c4899c3091da072e94a9d51d9ba45ba04
mikefab <mikefabrikant@gmail.com>
2018-11-25 17:08:53 -0500

    jwt token

--- server/config-sample.js
+++ server/config-sample.js
@@ -1,3 +1,9 @@
 module.exports = {
   default_path: './public/users/',
+  saveable: false,
+  authProviderDetails: {
+    iss: 'https://login.microsoftonline.com/48e05529-88b8-40e1-825a-18c4e1077b3a/v2.0/',
+    idp: 'UNICEF Azure AD'
+  },
+  whiteListedDomains: ['unicef.org']
 };
