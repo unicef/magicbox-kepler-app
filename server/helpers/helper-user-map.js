08d0af9ae7a050ae802f1b60020775162ff46edb
mikefab <mikefabrikant@gmail.com>
2018-11-25 17:08:53 -0500

    jwt token

--- server/helpers/helper-user-map.js
+++ server/helpers/helper-user-map.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const util = require('util');
-
+const config = require('../config')
 const mkdir = util.promisify(fs.mkdir);
 const jsonfile = require('jsonfile');
 const path = require('../config').default_path;
+const decode = require('jwt-decode')
 
 function create_user_dir(email) {
   return new Promise((resolve, reject) => {
@@ -23,6 +24,32 @@ function get_user_map(email) {
 }
 
 module.exports = {
+  checkTokenIsValid: (jwt) => {
+    const decodedToken = decode(jwt)
+    const {email, idp, exp, iss} = decodedToken
+
+    // Check token expiration
+    const current_time = new Date().getTime() / 1000;
+    if (current_time > exp) {
+      return false
+    }
+    // Check idp and iss match
+    if (
+      idp !== config.authProviderDetails.idp &&
+      iss !== config.authProviderDetails.iss
+    ) { return false}
+    // Check email domain
+    const emailDomain = email.split(/@/)[1]
+    if (
+      config.whiteListedDomains.filter(domain => {
+        return emailDomain === domain
+      }).lenth === -1
+    ) {
+      return false
+    }
+    return decodedToken.email
+  },
+
   check_user: email => new Promise((resolve, reject) => {
     // Check user has directory
     fs.stat(path + email, (err, stat) => {
--- server/routes/maps.js
+++ server/routes/maps.js
@@ -1,37 +1,61 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const config = require('../config')
 const jsonfile = require('jsonfile');
 const default_map = require('../public/users/default/config');
 const helper = require('../helpers/helper-user-map');
 
-router.get('/default/:email', (req, res) => {
+router.get('/default/:token', (req, res) => {
   if (req.params) {
-    helper.check_user(req.params.email)
-      .then((obj) => {
-        if (obj) {
-          res.send(obj);
-        } else {
+    const tokenIsValidThenEmail = helper.checkTokenIsValid(config.params.token)
+    if (tokenIsValidThenEmail) {
+      helper.check_user(tokenIsValidThenEmail)
+        .then((obj) => {
+          if (obj) {
+            res.send(obj);
+          } else {
+            res.send(default_map);
+          }
+        })
+        .catch(err => {
+          console.log(err);
           res.send(default_map);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.send(default_map);
-      });
+        });
+    // User is not authorized
+    } else {
+      console.log("DEFAULT")
+      res.send(default_map);
+    }
+
   }
 });
 
-router.route('/save/:email')
+router.route('/save/:token')
   .post((req, res) => {
-    jsonfile.writeFile(`./public/users/${req.params.email}/` + 'config.json', req.body, (err) => {
+
+    if (!config.saveable) {
+      return res.send({
+        message: 'Sorry, you cannot save a map'
+      });
+    }
+
+    const tokenIsValidThenEmail = helper.checkTokenIsValid(config.params.token)
+
+    if (!tokenIsValidThenEmail) {
+      return res.send({
+        message: 'Error: you are not authorized to save a map'
+      });
+    }
+
+    jsonfile.writeFile(`./public/users/${tokenIsValidThenEmail}/` + 'config.json', req.body, (err) => {
       if (err) {
         console.log(err);
         res.send({ message: 'Could not save' });
       } else {
         res.send({
           message: 'Saved! You may need to repoen your '
-            + 'browser in incognito mode next time you retrieve.',
+            + 'browser in incognito mode next time you retrieve.'
         });
       }
     });
