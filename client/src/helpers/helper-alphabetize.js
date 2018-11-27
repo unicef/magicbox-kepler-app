368a1a05cd0908f57e65a295000358990f5f8971
Marcella <marcella@Marcellas-MacBook-Air.local>
2018-11-27 10:36:13 -0500

    WIP refactoring: create helper function for sorting country lists

---
+++ client/src/helpers/helper-alphabetize.js
@@ -0,0 +1,11 @@
+module.exports = {
+  alphabetize_list: (list, nameKey) => {
+    list.sort((a, b) => {
+      let codeA = a.nameKey.toLowerCase();
+      let codeB = b.nameKey.toLowerCase();
+      if (codeA > codeB) return 1;
+      if (codeA < codeB) return -1;
+      return 0;
+    });
+  }
+}
