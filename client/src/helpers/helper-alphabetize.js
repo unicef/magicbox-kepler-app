c9942db3025f2af1d938656978450b37eed0c6e3
Anh3h <courageangeh@gmail.com>
2018-11-27 23:18:19 +0100

    Fix minor bugs in the refactored code

--- client/src/helpers/helper-alphabetize.js
+++ client/src/helpers/helper-alphabetize.js
@@ -1,8 +1,8 @@
 module.exports = {
   alphabetize_list: (list, nameKey) => {
     list.sort((a, b) => {
-      let codeA = a.nameKey.toLowerCase();
-      let codeB = b.nameKey.toLowerCase();
+      let codeA = a[nameKey].toLowerCase();
+      let codeB = b[nameKey].toLowerCase();
       if (codeA > codeB) return 1;
       if (codeA < codeB) return -1;
       return 0;
