85b3cf2f5a55fe07a5317277898a2790611bb909
mikefab <mikefabrikant@gmail.com>
2018-12-14 15:20:59 -0500

    sample mobility

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ cp ./client/.env-sample ./client/.env
 cp ./server/config-sample.js ./server/config.js
 cp ./server/test/config/config-sample.js ./server/test/config/config.js
 cp ./server/azure/config-sample.js ./server/azure/config.js
+cp ./server/public/mobility/2017-12-25-sample.csv ./server/public/mobility/2017-12-25.csv
 cp ./server/public/users/default/config-sample.json ./server/public/users/default/config.json
 cp ./client/config-sample.js ./client/config.js
