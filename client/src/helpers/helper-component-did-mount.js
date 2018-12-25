e11cf301957b8b4f1f6244aa1269434ef8c13d1a
mikefab <mikefabrikant@gmail.com>
2018-12-25 13:18:36 -0500

    Code cleanup

--- client/src/helpers/helper-component-did-mount.js
+++ client/src/helpers/helper-component-did-mount.js
@@ -19,17 +19,22 @@ module.exports = {
       .then(response => response.json())
       .then(obj => {
         if (!obj.error) {
-          let dataSets = {datasets: obj.datasets.map(s => { return {
-            info: {
-              id: s.data.id,
-              label: s.data.label,
-              color: s.data.color
-            },
-            data: {
-              fields: s.data.fields,
-              rows: s.data.allData
-            }
-          }}), config: obj.config}
+          let dataSets = {
+            datasets: obj.datasets.map(s => {
+              return {
+                info: {
+                  id: s.data.id,
+                  label: s.data.label,
+                  color: s.data.color
+                },
+                data: {
+                  fields: s.data.fields,
+                  rows: s.data.allData
+                }
+              }
+            }),
+          config: obj.config
+        }
 
           // addDataToMap action to inject dataset into kepler.gl instance
           props.dispatch(addDataToMap(dataSets))
